df668c657658a7c5d10e16749b3b149365b2e3ec
Leo <leorent@provideit.se>
2018-07-24 18:56:50 +0200

    Added event listeners

--- app.js
+++ app.js
@@ -6,17 +6,63 @@ var budgetController = (function(){
 //UI CONTROLLER
 var UIController = (function(){
 
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn',
+    };
+
+    return{
+        getInput: function(){
+            var type = document.querySelector(DOMStrings.inputType).value;
+            var description = document.querySelector(DOMStrings.inputDescription).value;
+            var value = document.querySelector(DOMStrings.inputValue).value;
+            return{
+                type: type,
+                description: description,
+                value: value,
+            }
+        },
+        getDOMstrings: function(){
+            return DOMStrings;
+        }
+    }
 })();
 
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
-    document.querySelector('.add__btn').addEventListener('click', function(){
-         // get the field input data
+
+    var setupEventListeners = function(){
+
+        var DOM = UIController.getDOMstrings();
+
+        document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(e){
+            if(e.keyCode === 13 || e.whitch === 13){
+               ctrlAddItem();
+            }
+        });
+    };
+
+
+
+    var ctrlAddItem  = function(){
+          // get the field input data
+          var input = UIController.getInput();
          // add the item to the budget controller
          // add the new item to user interface
          // calculate the budged
          // display the budget
+    }
 
-
-    });
+    return{
+        init: function(){
+            console.log('Application started');
+            setupEventListeners();
+        }
+    }
 })(budgetController, UIController);
+
+controller.init();
