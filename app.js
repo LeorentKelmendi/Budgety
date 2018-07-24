7cca2d75023c441b07425295234d3c4a20d1a99a
Leo <leorent@provideit.se>
2018-07-24 18:33:58 +0200

    Implementing module pattern

---
+++ app.js
@@ -0,0 +1,22 @@
+//BUDGET CONTROLLER
+var budgetController = (function(){
+
+})();
+
+//UI CONTROLLER
+var UIController = (function(){
+
+})();
+
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+    document.querySelector('.add__btn').addEventListener('click', function(){
+         // get the field input data
+         // add the item to the budget controller
+         // add the new item to user interface
+         // calculate the budged
+         // display the budget
+
+
+    });
+})(budgetController, UIController);
