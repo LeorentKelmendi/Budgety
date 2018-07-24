b31ac23ed8ea281baafcf5a8a8fbc5b9f9a225b3
Leo <leorent@provideit.se>
2018-07-24 19:18:14 +0200

    store user data

--- app.js
+++ app.js
@@ -1,6 +1,53 @@
 //BUDGET CONTROLLER
 var budgetController = (function(){
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.value = value;
+        this.description = description;
+    };
 
+    var Income = function(id, description, value){
+        this.id = id;
+        this.value = value;
+        this.description = description;
+    };
+
+    var data = {
+        allItems:{
+            expense: [],
+            income: [],
+        },
+        totals:{
+            expense: 0,
+            income: 0
+        }
+    };
+
+    return{
+        addItem: function(type, des, val){
+
+            var newItem,ID;
+            //create new id
+            if(data.allItems[type].length > 0){
+              ID = data.allItems[type][data.allItems[type].length - 1 ].id + 1;
+            }else{
+              ID = 0;
+            }
+            //create new item
+            if(type === 'expense'){
+                newItem = new Expense(ID, des, val);
+            }else if (type === 'income'){
+                newItem = new Income(ID, des, val);
+            }
+
+            //push to items and return.
+            data.allItems[type].push(newItem);
+            return newItem;
+        },
+        testing: function(){
+            console.log(data);
+        }
+    }
 })();
 
 //UI CONTROLLER
@@ -52,6 +99,7 @@ var controller = (function(budgetCtrl, UICtrl){
           // get the field input data
           var input = UIController.getInput();
          // add the item to the budget controller
+          var newItem = budgetController.addItem(input.type, input.description, input.value);
          // add the new item to user interface
          // calculate the budged
          // display the budget
