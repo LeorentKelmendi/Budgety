3afff923b66f3d3903f819462d3c6524025cd47a
Leo <leorent@provideit.se>
2018-07-24 20:12:05 +0200

    show the user input

--- app.js
+++ app.js
@@ -58,6 +58,8 @@ var UIController = (function(){
         inputDescription: '.add__description',
         inputValue: '.add__value',
         inputButton: '.add__btn',
+        incomeContainer:'.income__list',
+        expensesContainer: '.expenses__list'
     };
 
     return{
@@ -73,6 +75,23 @@ var UIController = (function(){
         },
         getDOMstrings: function(){
             return DOMStrings;
+        },
+        addListItem: function(obj, type){
+            var html, newHtml, element, finalHtml;
+
+            if(type === 'income'){
+            element = DOMStrings.incomeContainer;
+            html ='<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }else{
+            element = DOMStrings.expensesContainer;
+            html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%',  obj.description);
+            finalHtml = newHtml.replace('%value%',  obj.value);
+
+            document.querySelector(element).insertAdjacentHTML('beforeend', finalHtml);
         }
     }
 })();
@@ -101,6 +120,7 @@ var controller = (function(budgetCtrl, UICtrl){
          // add the item to the budget controller
           var newItem = budgetController.addItem(input.type, input.description, input.value);
          // add the new item to user interface
+          UIController.addListItem(newItem, input.type);
          // calculate the budged
          // display the budget
     }
